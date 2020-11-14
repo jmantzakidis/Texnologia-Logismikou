c72e549e78b771cd8de41eb9b3d9bf12810b4c1e
imantzakidis <johnymantzakidis@gmail.com>
2020-11-14 17:40:03 +0200

    Commit

---
+++ deleteemployee/src/main/java/com/example/deleteemployee/DelEmployee.java
@@ -0,0 +1,14 @@
+package com.example.deleteemployee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DelEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_del_employee);
+    }
+}
