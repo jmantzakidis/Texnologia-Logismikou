66619325d57ec336245d5ec2473c9770b83fba84
ManuelKuci <manouel28@gmail.com>
2020-12-01 23:33:44 +0200

    Restrictions form back button

--- app/src/main/java/com/example/loginproject/Restrictions.java
+++ app/src/main/java/com/example/loginproject/Restrictions.java
@@ -1,4 +1,23 @@
 package com.example.loginproject;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
-public class Restrictions {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Restrictions extends AppCompatActivity {
+    private Button backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restrictions);
+        backButton=findViewById(R.id.bt_back);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Restrictions.this,BossActivity.class);
+            startActivity(intent);
+        });
+    }
+
 }
