f23d9e5f6b0bd7016df8b68f39c5e470c689ddcd
GiannisAulo <johnaulogiaris@gmail.com>
2020-12-09 20:39:01 +0200

    New Branch

--- app/src/main/java/com/example/loginproject/UserDataAdapter.java
+++ app/src/main/java/com/example/loginproject/UserDataAdapter.java
@@ -37,7 +37,7 @@ public class UserDataAdapter extends RecyclerView.Adapter<UserDataAdapter.ViewHo
         ViewHolder viewHolder = new ViewHolder(userDataView);
         return viewHolder;
     }
-
+//ttes
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
